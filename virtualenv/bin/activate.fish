d5599c520998f103380df3cc18987f67a84012c8
core <core@localhost.localdomain>
2018-03-29 09:35:32 -0400

    test

--- virtualenv/bin/activate.fish
+++ virtualenv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/test/440wiki/virtualenv"
+set -gx VIRTUAL_ENV "/home/core/440wiki/virtualenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
